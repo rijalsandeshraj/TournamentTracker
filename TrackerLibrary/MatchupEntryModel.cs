1dae4f1e359f4afca76268ea4c60383e0dadf519
Sandesh Rijal <rijalsandeshraj@gmail.com>
2023-02-01 17:23:18 +0545

    Added Form

--- TrackerLibrary/MatchupEntryModel.cs
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TrackerLibrary
+﻿namespace TrackerLibrary
 {
+    /// <summary>
+    /// Represents one team in a matchup.
+    /// </summary>
     public class MatchupEntryModel
     {
         /// <summary>
--- TrackerLibrary/PersonModel.cs
+++ TrackerLibrary/PersonModel.cs
@@ -6,11 +6,29 @@ using System.Threading.Tasks;
 
 namespace TrackerLibrary
 {
+    /// <summary>
+    /// Represents one person.
+    /// </summary>
     public class PersonModel
     {
+        /// <summary>
+        /// The first name of the person.
+        /// </summary>
         public string FirstName { get; set; }
+
+        /// <summary>
+        /// The last name of the person.
+        /// </summary>
         public string LastName { get; set; }
+
+        /// <summary>
+        /// The primary email address of the person.
+        /// </summary>
         public string EmailAddress { get; set; }
+
+        /// <summary>
+        /// The phone number of the person.
+        /// </summary>
         public string PhoneNumber { get; set; }
     }
 }
